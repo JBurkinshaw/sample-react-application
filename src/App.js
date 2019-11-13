c7736997ea1a5b33a1f2f47c66400be1b78c0c09
Joe Burkinshaw <1443875+JBurkinshaw@users.noreply.github.com>
2019-11-13 11:48:08 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/*
+
+  App.js is used to control all React Context Providers
+
+*/
+
+import React from 'react'
+import { Layout } from './components'
+import './App.css'
+
+function App() {
+  return <Layout />
+}
+
+export default App
